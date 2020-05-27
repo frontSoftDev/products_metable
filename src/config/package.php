092e0b790ccb9c8450a68ff8c8345cc68a2b5b48
Marin <marin@dev.nddesign.no>
2020-05-27 11:11:18 +0300

    no interaction cmds and psr4 fixes

--- src/config/package.php
+++ src/config/package.php
@@ -1,9 +1,9 @@
 <?php
 	return [
 		'install' => [
-            'php artisan db:seed --class="\Marinar\ProductsMetable\Database\Seeds\MarinarProductsMetableSeeder"'
+            'php artisan db:seed --class="\Marinar\ProductsMetable\Database\Seeds\MarinarProductsMetableSeeder" -n'
 		],
 		'remove' => [
-            'php artisan db:seed --class="\Marinar\ProductsMetable\Database\Seeds\MarinarProductsMetableRemoveSeeder"'
+            'php artisan db:seed --class="\Marinar\ProductsMetable\Database\Seeds\MarinarProductsMetableRemoveSeeder" -n'
 		]
 	];
